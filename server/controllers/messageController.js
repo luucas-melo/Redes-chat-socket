3e08646b504158fe1b44197c598f6f7b3f7bec3d
Lucas Melo <lucas@pop-os.localdomain>
2022-04-30 17:11:03 -0300

    add messages controller

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,9 +1,17 @@
+const Messages = require('../models/Messages');
+
 module.exports = {
   async addMessage(req, res, next) {
     try {
+      console.log('MESSAGE', req.body);
       const { from, to, message } = req.body;
-
-      if (message) return res.status(200).json({ message });
+      const messageData = await Messages.create({
+        message: { text: message },
+        users: [from, to],
+        sender: from,
+      });
+      console.log(messageData);
+      if (messageData) return res.status(200).json({ messageData });
       else return res.json({ msg: 'Invalid message' });
     } catch (ex) {
       next(ex);
